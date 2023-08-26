b5ab95c853b298b3c5855661621446af83a65e2d
Iydon Liang <liangiydon@gmail.com>
2023-08-25 21:10:08 -0400

    :hammer: Add script/mtx.py

---
+++ script/mtx.py
@@ -0,0 +1,14 @@
+import pathlib as p
+import shutil
+
+
+cache = p.Path(__file__).parents[1] / 'cache'
+src = cache / 'tutorial'
+dst = cache / 'mtx'
+
+for path in src.rglob('controlDict'):
+    mtx = path.parents[1] / 'mtx'
+    directory = dst / mtx.relative_to(src).parent
+    if not mtx.exists() or directory.exists():
+        continue
+    shutil.copytree(mtx, directory)
